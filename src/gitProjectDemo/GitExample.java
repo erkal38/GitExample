92112106d833630f0c512c73b463ce4b77ae3902
erkal38 <nalruvceysem1984@gmail.com>
2021-03-13 19:50:11 +0600

    hello

--- src/gitProjectDemo/GitExample.java
+++ src/gitProjectDemo/GitExample.java
@@ -3,8 +3,7 @@ package gitProjectDemo;
 public class GitExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+          System.out.println("selam");
 	}
 
 }
